1466c58b142c8a1138f84b2b937d7627d6fabe8a
Dan Forest <iacchus.music@googlemail.com>
2025-04-27 11:04:13 +0100

    Update to remove the weather forcast front end and just leave the media player

--- cloudmediaplayer.client/src/app/app.component.ts
+++ cloudmediaplayer.client/src/app/app.component.ts
@@ -1,42 +1,17 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
+import { MediaPlayerComponent } from './media-player/media-player.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, MediaPlayerComponent]
 })
-export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
-
-  constructor(private http: HttpClient, public router: Router) { }
-
-  ngOnInit() {
-    this.getForecasts();
-  }
-
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
-
+export class AppComponent {
   title = 'cloudmediaplayer.client';
+
+  constructor(private http: HttpClient) { }
 }
--- cloudmediaplayer.client/src/app/app.module.ts
+++ cloudmediaplayer.client/src/app/app.module.ts
@@ -2,17 +2,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MediaPlayerComponent } from './media-player/media-player.component';
 
 @NgModule({
-  declarations: [],  // AppComponent removed from declarations
+  declarations: [],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule,
-    AppComponent,  // AppComponent added to imports
+    AppComponent,
     MediaPlayerComponent
   ],
   providers: [],
